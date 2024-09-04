814f1db0f1790c08f73adc54acfc7bdc5bfd8fba
김경빈 <kybin.kim@gmail.com>
2024-09-04 17:54:16 +0900

    [feat] 로그인 기능 구현

---
+++ android/HealWeGo/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "healwego7ba5a11d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
