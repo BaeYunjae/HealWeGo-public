6a3f58b40e40ef62a4f44eacfd06143219a6b602
배윤재 <baeyj1010@naver.com>
2024-09-10 10:26:27 +0900

    [feat] 결제 완료 확인 페이지

---
+++ android/HealWeGo/app/src/main/java/com/example/healwego/PaymentCompleteActivity.java
@@ -0,0 +1,13 @@
+package com.example.healwego;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PaymentCompleteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.finish_pay); // finish_pay.xml 파일 연결
+    }
+}
