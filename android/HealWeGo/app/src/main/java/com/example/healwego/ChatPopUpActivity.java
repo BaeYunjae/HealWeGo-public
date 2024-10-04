88e3000a66793ebc5dc94b9b6509e5de26d5eefe
김경빈 <kybin.kim@gmail.com>
2024-10-04 12:39:15 +0900

    [refactor] 인텐트 데이터 받기, 넘겨주기 추가

--- android/HealWeGo/app/src/main/java/com/example/healwego/ChatPopUpActivity.java
+++ android/HealWeGo/app/src/main/java/com/example/healwego/ChatPopUpActivity.java
@@ -34,12 +34,25 @@ public class ChatPopUpActivity extends AppCompatActivity {
     private TextView yesBtn;
     private TextView noBtn;
 
+    private String roomTitle;
+    private String usersInfo;
+    private String hostId;
+    private String roomId;
+    Boolean afterCreate;
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 바 제거
         setContentView(R.layout.activity_chat_popup);
 
         userId = AWSMobileClient.getInstance().getUsername();
+
+        Intent intent = getIntent();
+        roomTitle = intent.getStringExtra("roomTitle");
+        usersInfo = intent.getStringExtra("usersInfo");
+        hostId = intent.getStringExtra("hostId");
+        roomId = intent.getStringExtra("roomId");
+        afterCreate = intent.getBooleanExtra("isHost", false);
 
         yesBtn = (TextView)findViewById(R.id.yesBtn);
         noBtn = (TextView)findViewById(R.id.noBtn);
@@ -54,11 +67,7 @@ public class ChatPopUpActivity extends AppCompatActivity {
         noBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                // cancelPopUp으로 이동
-                Intent intent = new Intent(ChatPopUpActivity.this, ChatActivity.class);
-                intent.putExtra("result", 0);
-                startActivity(intent);
-                finish();
+                toChatActivity(0);
             }
         });
     }
@@ -105,12 +114,7 @@ public class ChatPopUpActivity extends AppCompatActivity {
                         } catch (JSONException e) {
                             throw new RuntimeException(e);
                         }
-
-                        // 방 리스트로 화면 전환
-                        Intent chatIntent = new Intent(ChatPopUpActivity.this, ChatActivity.class);
-                        chatIntent.putExtra("result", 1);
-                        startActivity(chatIntent);
-                        finish();
+                        toChatActivity(1);
                     } else {
                         // DELETE 요청 실패 시 처리
                         Log.e("ChatActivity", "DELETE 요청 실패: " + statusCode);
@@ -118,14 +122,26 @@ public class ChatPopUpActivity extends AppCompatActivity {
                     }
                 } catch (JSONException e) {
                     Log.e("ChatActivity", "DELETE 응답 처리 오류", e);
-                    Intent chatIntent = new Intent(ChatPopUpActivity.this, ChatActivity.class);
-                    chatIntent.putExtra("result", 0);
-                    startActivity(chatIntent);
-                    finish();
+                    toChatActivity(0);
                 }
             }
         }, bodyJson);
 
         Log.i("ChatPopUpActivity", "DELETE 요청: " + bodyJson);
+    }
+
+    // 방 리스트로 화면 전환
+    private void toChatActivity(int result){
+        Log.i("USER_ID", "popup : " + roomId);
+        Intent chatIntent = new Intent(ChatPopUpActivity.this, ChatActivity.class);
+        chatIntent.putExtra("result", result);
+        chatIntent.putExtra("isNewEnter", 0);
+        chatIntent.putExtra("roomTitle", roomTitle);
+        chatIntent.putExtra("usersInfo", usersInfo);
+        chatIntent.putExtra("hostId", hostId);
+        chatIntent.putExtra("roomId", roomId);
+        chatIntent.putExtra("afterCreate", afterCreate);
+        startActivity(chatIntent);
+        finish();
     }
 }
