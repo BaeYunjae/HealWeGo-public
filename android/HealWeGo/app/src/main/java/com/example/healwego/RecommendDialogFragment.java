7708484015e08b44e06b0b41d40102ca07469b65
김경빈 <kybin.kim@gmail.com>
2024-10-07 18:57:57 +0900

    [refactor] 소제목, 이미지 다이얼로그에 추가

--- android/HealWeGo/app/src/main/java/com/example/healwego/RecommendDialogFragment.java
+++ android/HealWeGo/app/src/main/java/com/example/healwego/RecommendDialogFragment.java
@@ -1,10 +1,25 @@
 package com.example.healwego;
 
 import android.app.Dialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Outline;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewOutlineProvider;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,11 +29,14 @@ import androidx.fragment.app.DialogFragment;
 public class RecommendDialogFragment extends DialogFragment {
 
     TextView title, txtText;
+    ImageView locImg;
 
-    public static RecommendDialogFragment newInstance(String locName, String description) {
+    public static RecommendDialogFragment newInstance(byte[] locImg, String locName, String subtitle, String description) {
         RecommendDialogFragment fragment = new RecommendDialogFragment();
         Bundle args = new Bundle();
+        args.putByteArray("locImg", locImg);
         args.putString("locName", locName);
+        args.putString("subtitle", subtitle);
         args.putString("description", description);
         fragment.setArguments(args);
         return fragment;
@@ -33,14 +51,18 @@ public class RecommendDialogFragment extends DialogFragment {
         LayoutInflater inflater = requireActivity().getLayoutInflater();
         View dialogView = inflater.inflate(R.layout.dialog_recommend, null);
 
-        title = dialogView.findViewById(R.id.title);
+        locImg = dialogView.findViewById(R.id.locImage);
+        title = dialogView.findViewById(R.id.titleText);
         txtText = dialogView.findViewById(R.id.txtText);
 
         if (getArguments() != null) {
-            String locName = getArguments().getString("locName");
+            String subtitle = getArguments().getString("subtitle");
             String description = getArguments().getString("description");
+            byte[] decodedString = getArguments().getByteArray("locImg");
+            Bitmap decodedImage = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
 
-            title.setText(locName);
+            locImg.setImageBitmap(decodedImage);
+            title.setText(subtitle);
             txtText.setText(description);
         }
 
